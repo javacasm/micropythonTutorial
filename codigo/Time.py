30042868524eff725383a948a45035754bffe223
Javacasm <javacasm@gmail.com>
2019-01-26 21:26:33 +0100

    update

--- codigo/Time.py
+++ codigo/Time.py
@@ -7,3 +7,12 @@ time.sleep_ms(500)      # sleep for 500 milliseconds
 time.sleep_us(10)       # sleep for 10 microseconds
 start = time.ticks_ms() # get millisecond counter
 delta = time.ticks_diff(time.ticks_ms(), start) # compute time difference
+
+import utime
+
+days= {0:"Monday", 1:"Tuesday", 2:"Wednesday", 3:"Thursday", 4:"Friday", 5:"Saturday", 6:"Sunday"}
+months = {1:"January", 2:"February", 3:"March", 4:"April", 5:"May", 6:"June", 7:"July", 8:"August", 9:"September", 10:"October", 11:"November", 12:"December"}
+(year, month, mday, hour, minute, second, weekday, yearday) = utime.localtime()
+
+print("Today is {}".format(days[weekday]))
+print("The month is {}".format(months[month]))

df85997a2e6604912aee234c6a947b1c1f9a9534
Javacasm <javacasm@gmail.com>
2019-01-26 19:46:36 +0100

    add

---
+++ codigo/I2C.py
@@ -0,0 +1,26 @@
+# https://docs.micropython.org/en/latest/esp8266/quickref.html#i2c-bus
+
+from machine import Pin, I2C
+
+# construct an I2C bus
+i2c = I2C(scl=Pin(5), sda=Pin(4), freq=100000) # Se puede usar cualquier pin
+
+print('Scan i2c bus...')
+devices = i2c.scan()
+
+if len(devices) == 0:
+  print("No i2c device !")
+else:
+  print('i2c devices found:',len(devices))
+
+  for device in devices:  
+    print("Decimal address: ",device," | Hexa address: ",hex(device))
+    
+
+i2c.readfrom(0x3a, 4)   # read 4 bytes from slave device with address 0x3a
+i2c.writeto(0x3a, '12') # write '12' to slave device with address 0x3a
+
+buf = bytearray(10)     # create a buffer with 10 bytes
+i2c.writeto(0x3a, buf)  # write the given buffer to the slave
+
+

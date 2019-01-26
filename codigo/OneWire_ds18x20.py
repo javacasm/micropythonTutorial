c536448e46e3dbf5d4debc1dfcfd4f9db4fe7d88
Javacasm <javacasm@gmail.com>
2019-01-26 19:46:43 +0100

    add

---
+++ codigo/OneWire_ds18x20.py
@@ -0,0 +1,25 @@
+# https://docs.micropython.org/en/latest/esp8266/quickref.html#deep-sleep-mode
+
+# You must also power the sensors and connect a 4.7k Ohm resistor between the data pin and the power pin.    
+import time
+import machine
+import onewire, ds18x20
+
+# the device is on GPIO12
+dat = machine.Pin(12)
+
+# create the onewire object
+ds = ds18x20.DS18X20(onewire.OneWire(dat))
+
+# scan for devices on the bus
+roms = ds.scan()
+print('found devices:', roms)
+
+# loop 10 times and print all temperatures
+for i in range(10):
+    print('temperatures:', end=' ')
+    ds.convert_temp()
+    time.sleep_ms(750)
+    for rom in roms:
+        print(ds.read_temp(rom), end=' ')
+    print()

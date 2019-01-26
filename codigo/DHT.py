b95d4980e5006ae6a4898aa3ea18dea4d7b0f3a5
Javacasm <javacasm@gmail.com>
2019-01-26 19:46:08 +0100

    add

---
+++ codigo/DHT.py
@@ -0,0 +1,13 @@
+# https://docs.micropython.org/en/latest/esp8266/quickref.html#dht-driver
+
+
+# DHT11 can be called no more than once per second and the DHT22 once every two seconds for most accurate results.
+
+import dht
+import machine
+d11 = dht.DHT11(machine.Pin(4))
+
+d22 = dht.DHT22(machine.Pin(5))
+d11.measure()
+d11.temperature()
+d11.humidity()

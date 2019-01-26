00c2cb84e6137bdb0b547770e95c0549732c0a84
Javacasm <javacasm@gmail.com>
2019-01-26 19:47:30 +0100

    add

---
+++ codigo/Timer.py
@@ -0,0 +1,9 @@
+# https://docs.micropython.org/en/latest/esp8266/quickref.html#timers
+
+from machine import Timer
+
+tim = Timer(-1)
+tim.init(period=5000, mode=Timer.ONE_SHOT, callback=lambda t:print(1))
+tim.init(period=2000, mode=Timer.PERIODIC, callback=lambda t:print(2))
+
+

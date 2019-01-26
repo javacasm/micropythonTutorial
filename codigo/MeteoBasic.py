d1261d8eff9dbe6710410508aeea5c510f054b8f
Javacasm <javacasm@gmail.com>
2019-01-26 21:26:30 +0100

    update

--- codigo/MeteoBasic.py
+++ codigo/MeteoBasic.py
@@ -5,6 +5,10 @@ import machine,bme280
 from machine import I2C
 from esp8266_i2c_lcd import I2cLcd 
 
+from ntptime import settime
+settime()  ## Ajusta el tiempo
+rtc = machine.RTC()
+
 i2c = machine.I2C(scl=machine.Pin(5), sda=machine.Pin(4)) 
 i2c.scan() ## [63, 118] 
 bme = bme280.BME280(i2c=i2c) 
@@ -17,3 +21,15 @@ lcd.move_to(0, 1)
 lcd.putstr(bme.values[1])
 lcd.move_to(10, 0)
 lcd.putstr(bme.values[2]) 
+
+(year, month, mday, hour, minute, second, weekday, yearday) = utime.localtime()
+
+lcd.move_to(10,1)
+if hour < 10 :
+    lcd.putsrt('0')
+lcd.putsrt(':')
+
+if minute < 10 :
+    lcd.putsrt('0')
+lcd.putstr(str(minute))
+

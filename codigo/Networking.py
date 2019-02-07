8d83ebddd8953ad1180a184567062a39e2f2e958
Javacasm <javacasm@gmail.com>
2019-02-07 15:08:11 +0100

    update

--- codigo/Networking.py
+++ codigo/Networking.py
@@ -5,8 +5,8 @@ import network
 wlan = network.WLAN(network.STA_IF) # create station interface
 wlan.active(True)       # activate the interface
 wlan.scan()             # scan for access points
-wlan.isconnected()      # check if the station is connected to an AP
 wlan.connect('essid', 'password') # connect to an AP
+wlan.isconnected()      # check if the station is connected to an AP
 wlan.config('mac')      # get the interface's MAC adddress
 wlan.ifconfig()         # get the interface's IP/netmask/gw/DNS addresses
 

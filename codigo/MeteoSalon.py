a5df000d1db2c2954eb5a01f100c2c4859f3484d
Javacasm <javacasm@gmail.com>
2019-11-22 14:47:06 +0100

    update

--- codigo/MeteoSalon.py
+++ codigo/MeteoSalon.py
@@ -1,7 +1,38 @@
 # Meteo Salon
 
-# SDA       D4
-# SCL       D3
-# LED RGB   D2
+# SDA       D4 (GPIO02)
+# SCL       D3 (GPIO00)
+# LED RGB   D2 (GPIO04)
 
-ledRGB = neopixel.NeoPixel(machine.Pin(2),1) # Led RGB through the Hole en pin D4 (GPIO2)
+import neopixel,machine,NeoPixelTHO,time
+
+
+ledRGB = neopixel.NeoPixel(machine.Pin(4),1) # Led RGB through the Hole en pin D2 (GPIO4)
+led = machine.Pin(2,machine.Pin.OUT)  
+
+def testColor():
+    for color in NeoPixelTHO.ciclo:
+        ledRGB[0] = color
+        ledRGB.write()
+
+## Encendemos todo
+def encendemosTodo():
+    testColor()
+    led.off()
+
+## Apagamos todo
+def apagamosTodo():
+    led.on()
+    ledRGB[0]=NeoPixelTHO.black
+    ledRGB.write()
+
+def start():
+    encendemosTodo()
+    time.sleep(1000)
+    apagamosTodo()
+    
+
+
+
+
+

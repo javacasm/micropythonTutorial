c9fa649ef82b82beb3e467d2f7a93750e8df6b7c
Javacasm <javacasm@gmail.com>
2019-11-23 19:03:41 +0100

    add

--- codigo/MeteoSalon.py
+++ codigo/MeteoSalon.py
@@ -4,11 +4,12 @@
 # SCL       D3 (GPIO00)
 # LED RGB   D2 (GPIO04)
 
-import neopixel,machine,NeoPixelTHO,time
+import neopixel,machine,NeoPixelTHO,time,Wemos
 
 
-ledRGB = neopixel.NeoPixel(machine.Pin(4),1) # Led RGB through the Hole en pin D2 (GPIO4)
-led = machine.Pin(2,machine.Pin.OUT)  
+ledRGB = neopixel.NeoPixel(machine.Pin(Wemos.D2),1) # Led RGB through the Hole en pin D4 (GPIO4)
+led = machine.Pin(Wemos.BUILTIN_LED,machine.Pin.OUT)
+led.on()  # It's inverted
 
 def testColor():
     for color in NeoPixelTHO.ciclo:
@@ -18,6 +19,7 @@ def testColor():
 ## Encendemos todo
 def encendemosTodo():
     testColor()
+    time.sleep(1)
     led.off()
 
 ## Apagamos todo
@@ -28,9 +30,13 @@ def apagamosTodo():
 
 def start():
     encendemosTodo()
-    time.sleep(1000)
+    time.sleep(1)
     apagamosTodo()
-    
+
+def color(c):
+    ledRGB[0]=c
+    ledRGB.write()
+
 
 
 

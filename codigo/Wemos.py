7c380499f05b957a7cb90481e01f7f533f52bb86
Javacasm <javacasm@gmail.com>
2019-11-23 19:03:02 +0100

    add

---
+++ codigo/Wemos.py
@@ -0,0 +1,17 @@
+# Wemos-ESP12 Adapter
+# D3 = GPIO0  High = Run Low = Flash
+# D4 = GPIO2 Led BuiltIn ESP12
+# D0 = GPIO16 Wake	
+# A0 = ADC
+TX = 1
+RX = 3
+D1 = 5
+D2 = 4
+D3 = 0
+D4 = 2
+D0 = 16
+D5 = 14
+D6 = 12
+D7 = 13
+D8 = 15
+BUILTIN_LED = 2

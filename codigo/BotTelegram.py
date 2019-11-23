b7d06e46639d8102397ae29824cf1a8ab59e5bb1
Javacasm <javacasm@gmail.com>
2019-11-23 19:03:23 +0100

    add

---
+++ codigo/BotTelegram.py
@@ -0,0 +1,3 @@
+# Bot en python https://www.youtube.com/watch?v=73coNZ-5hWM
+# Conexion con Bot en python via MQTT https://www.youtube.com/watch?v=uXGPnbTiYXc
+#           Ejemplos https://github.com/gsampallo/micropython_examples

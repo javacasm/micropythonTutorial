7e22adb9bce0f407514acdd558a17dc1ab6cd1ef
Javacasm <javacasm@gmail.com>
2019-11-23 19:03:50 +0100

    add

--- codigo/NeoPixelTHO.py
+++ codigo/NeoPixelTHO.py
@@ -1,8 +1,6 @@
 import machine,neopixel
 
-# Using a wemos https://escapequotes.net/wp-content/uploads/2016/02/esp8266-wemos-d1-mini-pinout.png
-
-np = neopixel.NeoPixel(machine.Pin(2),1) # Led RGB through the Hole en pin D4 (GPIO2)
+# Led compatible neopixel https://a.pololu-files.com/picture/0J5433.631.jpg 
 
 blue = (0,0,150)
 green = (150,0,0) 
@@ -10,8 +8,11 @@ red = (0,150,0)
 black = (0,0,0) 
 white = (255,255,255)
 
-ciclo = {blue, green, red, white, black}
-for color in ciclo:
-    np[0] = color
-    np.write()
+
+def test(pin = 2):
+    np = neopixel.NeoPixel(machine.Pin(pin),1) # Led RGB through the Hole en pin D4 (GPIO2) by default
+    ciclo = {blue, green, red, white, black}
+    for color in ciclo:
+        np[0] = color
+        np.write()
 

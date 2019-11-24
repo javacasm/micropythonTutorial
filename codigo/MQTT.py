d63c300dc96f0efe2f6dae294d643284a7517839
Javacasm <javacasm@gmail.com>
2019-11-24 23:26:07 +0100

    update

--- codigo/MQTT.py
+++ codigo/MQTT.py
@@ -3,5 +3,5 @@
 # Referencias
 # modulo MQTT https://github.com/micropython/micropython-lib/tree/master/umqtt.simple
 # https://randomnerdtutorials.com/micropython-mqtt-esp32-esp8266/
-# Conexion con Bot en python via MQTT https://www.youtube.com/watch?v=uXGPnbTiYXc
+# Conexion con Bot en python via MQTT  
 #           Ejemplos https://github.com/gsampallo/micropython_examples

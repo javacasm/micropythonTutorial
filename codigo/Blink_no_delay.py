9273e73ca6c97e0cc0d05b9de1a02b093adb2e94
Javacasm <javacasm@gmail.com>
2019-12-05 14:30:07 +0100

    add

--- codigo/Blink_no_delay.py
+++ codigo/Blink_no_delay.py
@@ -1,3 +1,5 @@
+# Mirar async task https://github.com/peterhinch/micropython-async/blob/master/TUTORIAL.md
+
 import utime
 
 last_led = utime.ticks_ms()
@@ -6,4 +8,5 @@ while True:
     now = utime.ticks_ms()
     if utime.ticks_diff(now, last_led) > 500:
         last_led = now
+        
         

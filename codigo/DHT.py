bbb9206df7bb7be428b603f3a3744f358c285722
Javacasm <javacasm@gmail.com>
2019-12-05 14:29:43 +0100

    add

--- codigo/DHT.py
+++ codigo/DHT.py
@@ -3,6 +3,8 @@
 
 # DHT11 can be called no more than once per second and the DHT22 once every two seconds for most accurate results.
 
+# Wemos DHT11 Shield  uses D4
+
 import dht
 import machine
 d11 = dht.DHT11(machine.Pin(4))

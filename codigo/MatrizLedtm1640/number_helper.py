d8572b73dbadb1fe12d1c06b489759e1c7ce48da
Javacasm <javacasm@gmail.com>
2019-12-10 13:04:27 +0100

    update

--- codigo/MatrizLedtm1640/number_helper.py
+++ codigo/MatrizLedtm1640/number_helper.py
@@ -1,3 +1,7 @@
+import machine
+import time
+import tm1640
+import Wemos
 
 # Numbers 0 al 9 de 4x6
 
@@ -14,26 +18,38 @@ digits_4x6 = [
 0x0c52523c00000000,
 ]
 
-def createTM1640()
-    import machine
-    import tm1640
-    import Wemos
+digits_3x6 = [
+0x3c423c0000000000,
+0x447e400000000000,
+0x64524c0000000000,
+0x424a340000000000,
+0x1e107c0000000000,
+0x4e4a320000000000,
+0x3c4a320000000000,
+0x62120e0000000000,
+0x344a340000000000,
+0x040a7e0000000000
+]
+
+def createTM1640():
     tm = tm1640.TM1640(clk = machine.Pin(Wemos.D5), dio = machine.Pin(Wemos.D7))
     return tm
-    
+
 def count(tm ):
-    import time
-    import tm1640
     for i in range(10):
         tm.write_int(digits_4x6[i])
         time.sleep(1)
 
 def show2FiguresNumber(tm, numero):
-    import tm1640
     numero = int(numero)
     units = numero % 10
     tens = numero // 10
     tm.write_int(digits_4x6[tens])
     tm.write_int(digits_4x6[units],4)
     
-
+def show2FiguresNumberX3(tm, numero):
+    numero = int(numero)
+    units = numero % 10
+    tens = numero // 10
+    tm.write_int(digits_3x6[tens],1)
+    tm.write_int(digits_3x6[units],5)

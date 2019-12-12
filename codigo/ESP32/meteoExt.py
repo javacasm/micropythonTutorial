6084cd2569deb83f1cf71a543bf7a2b0974ff11a
Javacasm <javacasm@gmail.com>
2019-12-12 09:10:07 +0100

    update

--- codigo/ESP32/meteoExt.py
+++ codigo/ESP32/meteoExt.py
@@ -1,10 +1,15 @@
 import machine
 import BME280
 
+# v1.3
+
 def initSensor():
     i2c = machine.I2C(sda = machine.Pin(5),scl = machine.Pin(4))
     i2cDevices = i2c.scan()
     if len(i2cDevices) > 0:
         bme = BME280.BME280(i2c = i2c,address = 118)
         print(bme.temperature)
-    return bme
+        return bme
+    else:
+        print('No BME280 device found')
+        return None

fce0a02c70776c15037c256ba8c273dd5e0fc89c
Javacasm <javacasm@gmail.com>
2019-12-15 22:26:26 +0100

    add

---
+++ codigo/baseNodo/boot.py
@@ -0,0 +1,14 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+import uos, machine
+#uos.dupterm(None, 1) # disable REPL on UART(0)
+import gc
+import webrepl
+webrepl.start()
+
+import configNode
+configNode.initNode()
+print('Welcome to '+configNode.NodeName)
+
+gc.collect()

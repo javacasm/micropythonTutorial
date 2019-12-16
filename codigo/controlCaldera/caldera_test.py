1da097168344c4d34b8b27207c51816e4bb0eccc
Javacasm <javacasm@gmail.com>
2019-12-16 13:45:44 +0100

    mv

--- codigo/controlCaldera/caldera_test.py
+++ codigo/controlCaldera/caldera_test.py
@@ -1,18 +1,19 @@
 import machine,neopixel
+import Wemos
 
 # Using a wemos https://escapequotes.net/wp-content/uploads/2016/02/esp8266-wemos-d1-mini-pinout.png
 
 # Led compatible neopixel https://a.pololu-files.com/picture/0J5433.631.jpg 
 
-np = neopixel.NeoPixel(machine.Pin(2),1) # Led RGB through the Hole en pin D4 (GPIO2)
+np = neopixel.NeoPixel(machine.Pin(D4),1) # Led RGB through the Hole en pin D4 (GPIO2)
 
-blue = (0,0,50)
-green = (50,0,0) 
-red = (0,50,0) 
+blue = (0,0,10)
+green = (10,0,0) 
+red = (0,10,0) 
 black = (0,0,0) 
 white = (255,255,255)
 
-rele = machine.Pin(5,machine.Pin.OUT)  # Rele shield en D1 (GPIO5)
+rele = machine.Pin(Wemos.D1,machine.Pin.OUT)  # Rele shield en D1 (GPIO5)
 
 
 def enciendeCaldera():

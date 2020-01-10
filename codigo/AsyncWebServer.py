faef72f4183cfa5e68702fbed9eeb2c7ab034c60
Javacasm <javacasm@gmail.com>
2020-01-10 09:27:18 +0100

    update

--- codigo/AsyncWebServer.py
+++ codigo/AsyncWebServer.py
@@ -1,5 +1,21 @@
 # AsyncWebServer
 
-https://github.com/micropython/micropython-lib/blob/master/uasyncio/example_http_server.py  
+## uasyncio
+
+Usaremos [uasyncio](https://github.com/micropython/micropython-lib/tree/master/uasyncio)
+[Tutorial](https://github.com/peterhinch/micropython-async/blob/master/TUTORIAL.md)
+
+[Ejemplo de webserver](https://github.com/micropython/micropython-lib/blob/master/uasyncio/example_http_server.py)  
 
 https://forum.micropython.org/viewtopic.php?t=4828
+
+## uhttpd
+
+https://forum.micropython.org/viewtopic.php?f=16&t=2711&sid=7a2b5596ce021f2d220f6b06b3517505
+
+## ws
+
+
+https://forum.micropython.org/viewtopic.php?t=4010
+
+

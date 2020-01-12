20c05a001f78edfc7ad9e1d4376b1e3100500c48
Javacasm <javacasm@gmail.com>
2020-01-12 21:28:57 +0100

    update

--- codigo/PCD8544/pcd8544_test.py
+++ codigo/PCD8544/pcd8544_test.py
@@ -1,38 +1,66 @@
 # Ejemplo de uso de pantalla Nokia 5119 con pcd8544
 # Tomado de https://github.com/mcauser/MicroPython-ESP8266-DHT-Nokia-5110/blob/master/readme.md
+# https://github.com/mcauser/micropython-pcd8544
 
-
-from machine import Pin, SPI
-
-import time
 import pcd8544
+from machine import Pin, SPI
+import time
+lcd = None
+def init():
+    global lcd
+    spi = SPI(1)
+    spi.init(baudrate=2000000, polarity=0, phase=0)
+    cs = Pin(2)
+    dc = Pin(15)
+    rst = Pin(0)
 
-spi = SPI(1, baudrate=80000000, polarity=0, phase=0)
+    # backlight on
+    bl = Pin(12, Pin.OUT, value=1)
 
-cs = Pin(5)
-dc = Pin(15)
-rst = Pin(16)
+    lcd = pcd8544.PCD8544(spi, cs, dc, rst)
 
-bl = Pin(12, Pin.OUT, value=1) # Backlight On
-lcd = pcd8544.PCD8544(spi, cs, dc, rst)
+    lcd.init()
+    lcd.contrast(50)
+    
+def test():
+    global lcd
+    # bitmap smiley (horzontal msb)
+    lcd.clear()
+    # draw 8x16 in bank 0 (rows 0..7)
+    lcd.position(0, 0)
+    lcd.data(bytearray(b'\xE0\x38\xE4\x22\xA2\xE1\xE1\x61\xE1\x21\xA2\xE2\xE4\x38\xE0\x00'))
+    # draw 8x16 in bank 1 (rows 8..15)
+    lcd.position(0, 1)
+    lcd.data(bytearray(b'\x03\x0C\x10\x21\x21\x41\x48\x48\x48\x49\x25\x21\x10\x0C\x03\x00'))
+    time.sleep(2)
+    print('apagamos')
+    # toggle display, image persists in DDRAM
+    lcd.power_off()
+    time.sleep(2)
+    print('encendemos')
+    lcd.power_on()
 
-import framebuf
+def testFB():
 
-buffer = bytearray((lcd.height // 8) * lcd.width)
-framebuf = framebuf.FrameBuffer1(buffer, lcd.width, lcd.height)
+    # https://github.com/mcauser/MicroPython-ESP8266-DHT-Nokia-5110
+    global lcd
+    import framebuf
+    print('framebuffer')
+    buffer = bytearray((lcd.height // 8) * lcd.width)
+    fb = framebuf.FrameBuffer1(buffer, lcd.width, lcd.height)
 
-# Light every pixel:
+    # Light every pixel:
+    print('todo a 1')
+    fb.fill(1)
+    lcd.data(buffer)
+    time.sleep(2)
+    #Clear screen:
+    print('todo a 0')
+    fb.fill(0)
+    lcd.data(buffer)
+    time.sleep(2)
+    # Print Hello, World! using the 8x8 font:
 
-framebuf.fill(1)
-lcd.data(buffer)
-
-#Clear screen:
-
-framebuf.fill(0)
-lcd.data(buffer)
-
-# Print Hello, World! using the 8x8 font:
-
-framebuf.text("Hello,", 0, 0, 1)
-framebuf.text("World!", 0, 9, 1)
-lcd.data(buffer)
+    fb.text("Hello,", 0, 0, 1)
+    fb.text("World!", 0, 9, 1)
+    lcd.data(buffer)

de46cd7193d1d1d10308406fcd60f87b96e2d479
Javacasm <javacasm@gmail.com>
2020-01-13 10:28:32 +0100

    update

--- codigo/AsyncTask.py
+++ codigo/AsyncTask.py
@@ -1,2 +1,4 @@
 # Ejemplo https://github.com/kfricke/micropython-sonoff-switch
+# uasyncio https://github.com/micropython/micropython-lib/tree/master/uasyncio
+# Tutorial uasyncio https://github.com/peterhinch/micropython-async/blob/master/TUTORIAL.md#01-installing-uasyncio-on-bare-metal
 # Parpadeando varios leds https://github.com/peterhinch/micropython-async/blob/master/aledflash.py

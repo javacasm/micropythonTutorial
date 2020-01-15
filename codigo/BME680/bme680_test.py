a58ae619f3edaa5c59ad388a6299c1a2f077a480
Javacasm <javacasm@gmail.com>
2020-01-15 14:37:56 +0100

    test sensor bme680

---
+++ codigo/BME680/bme680_test.py
@@ -0,0 +1,5 @@
+# BME680: sensor temperature, humidity, barometric pressure, and VOC gas
+# https://www.adafruit.com/product/3660 
+# https://learn.adafruit.com/adafruit-bme680-humidity-temperature-barometic-pressure-voc-gas?view=all
+# https://github.com/robmarkcole/bme680-mqtt-micropython
+#

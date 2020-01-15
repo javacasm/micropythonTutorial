ff142108e8f5571baa48b29dd40fcfe59e76119b
Javacasm <javacasm@gmail.com>
2020-01-15 14:38:09 +0100

    test GML8511

---
+++ codigo/GYML8511/GYML8511_test.py
@@ -0,0 +1,18 @@
+"""
+
+GYML8511 sensor de luz ultravioleta 
+MP8511 https://cdn.sparkfun.com/datasheets/Sensors/LightImaging/ML8511_3-8-13.pdf
+https://learn.sparkfun.com/tutorials/ml8511-uv-sensor-hookup-guide/all
+
+
+Da una salida analógica
+http://wiki.sunfounder.cc/index.php?title=GYML8511_UV_Sensor
+![pinout](http://wiki.sunfounder.cc/images/6/63/GYML8511_2.png)
+ 
+VIN	Input terminal of the voltage before getting regulated
+3V3	The anode of the power supply 3v3
+GND	The cathode of the power supply
+OUT	Output (Low in power down or standby mode)
+EN	Active high enable pin. (High: Active mode, Low: Standby mode)
+
+"""

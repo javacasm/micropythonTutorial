39df5a5d00732510602dce1a113112576ecddc40
Javacasm <javacasm@gmail.com>
2020-01-31 14:35:08 +0100

    exception handling

--- codigo/controlCaldera/main.py
+++ codigo/controlCaldera/main.py
@@ -1,14 +1,13 @@
-v = '1.3.7'
+v = '1.3.8'
 
 import machine
-import myDateTime
-# import WebServerControlRele
-
-import MQTT_caldera
-
-print(myDateTime.setRTC())
 
 try:
+    import myDateTime
+    # import WebServerControlRele    
+    import MQTT_caldera
+    print(myDateTime.setRTC())
+
     MQTT_caldera.mainBeta()
 except Exception as e:
     print(str(e))

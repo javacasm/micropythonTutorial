cef526d22ba602829bee8bd9dd9a40c7dbdf396e
Javacasm <javacasm@gmail.com>
2020-02-04 14:27:49 +0100

    codigo de ejemplos y montajes con el ina219

---
+++ codigo/ina219_test.py
@@ -0,0 +1,3 @@
+# modulo https://github.com/chrisb2/pyb_ina219
+# Ejemplo https://learn.adafruit.com/build-a-cloud-connected-esp8266-power-meter?view=all
+# tutorial https://learn.adafruit.com/adafruit-ina219-current-sensor-breakout?view=all

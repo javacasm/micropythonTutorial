6ec82c388c405369983be76baef80a6f4f76e28e
Javacasm <javacasm@gmail.com>
2020-02-20 23:48:44 +0100

    add comment

--- codigo/Baterias/BateryLevel_ttgo_esp32.py
+++ codigo/Baterias/BateryLevel_ttgo_esp32.py
@@ -5,7 +5,7 @@ def batLevel():
     adcBat.atten(machine.ADC.ATTN_11DB) # rango de 3.6V
     adcBat.width(machine.ADC.WIDTH_12BIT)
     adcValue = adcBat.read()
-    voltBat = adcValue * 2 * 3.6 / 4095
+    voltBat = adcValue * 2 * 3.6 / 4095 # el 2 por el divisor de tension
     percentBat = voltBat*100/4.2
     print('%1.2fv - %d%%'%(voltBat,percentBat))
     return percentBat

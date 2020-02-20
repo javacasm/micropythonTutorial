77ef1ee13fc4c4dfbc6e7d8d26da3be2ff5d7f27
Javacasm <javacasm@gmail.com>
2020-02-21 00:21:48 +0100

    fix error

--- codigo/Baterias/mqtt_battery.py
+++ codigo/Baterias/mqtt_battery.py
@@ -1,16 +1,16 @@
 # MQTT test 
 # basado en https://randomnerdtutorials.com/micropython-mqtt-esp32-esp8266/
 
-v = '0.9'
+v = '0.9.3'
 
 from umqttsimple import MQTTClient
 import ubinascii
 import machine
 import time         # Para las esperas
 import utime
-import time
 import ntptime
 import MyDateTime
+import BateryLevel_ttgo_esp32
 
 
 client_id = ubinascii.hexlify(machine.unique_id())
@@ -22,37 +22,26 @@ topic_subLed = topic_sub + b'/ledTTYGO'
 
 mqtt_server = '192.168.1.100'
 
-# LedRGBN
-ledRGB = neopixel.NeoPixel(machine.Pin(17),1) # Led RGB through the Hole en pin GPIO17
-
-# Builtin Led
-led = machine.Pin(5,machine.Pin.OUT)
+# Encendemos la backlight de la pantalla
+backLight = machine.Pin(4,machine.Pin.OUT)
 
 def sub_CheckTopics(topic, msg):
     print((topic, msg))
     if topic == topic_subLed:     # Check for Led Topic
         if msg == b'On':
             print('Led:On')
-            led.off()
+            backLight.on()
         else:
             print('Led:Off')
-            led.on()
-    elif topic == topic_subLedRGB:      ## Check for RGB Topic
-        ledRGB[0]=NeoPixelTHO.colorByName(msg)
-        ledRGB.write()
-    elif topic == topic_subFree:        ## Check for free memory
-        freeMem = helpFiles.free()
-        client.publish(topic_subMem, str(freeMem))
+            backLight.off()
 
 def connect_and_subscribe():
-  global client, client_id, mqtt_server, topic_sub, topic_subLedRGB, topic_subLed
+  global client, client_id, mqtt_server, topic_sub,  topic_subLed
   client = MQTTClient(client_id, mqtt_server)
   client.set_callback(sub_CheckTopics)
   client.connect()
-  client.subscribe(topic_subFree)
   client.subscribe(topic_subLed)
-  client.subscribe(topic_subLedRGB)
-  print('Connected to %s MQTT broker, subscribed to %s topic' % (mqtt_server, topic_subFree))
+  print('Connected to %s MQTT broker, subscribed to %s topic' % (mqtt_server, topic_subLed))
   return client
 
 
@@ -61,21 +50,16 @@ def restart_and_reconnect():
   time.sleep(10)
   machine.reset()
 
-
 def mainBeta(everySeconds=60):
     print(MyDateTime.setRTC())
     connect_and_subscribe() # connect and get a client reference
     last_Temp = 0 # utime.ticks_ms()
-    bme = meteoExt.initSensor()
     while True :
         client.check_msg() # Check por new messages and call the callBack function
         now = utime.ticks_ms()
         if utime.ticks_diff(now, last_Temp) > (everySeconds*1000):
-            client.publish(topic_subTime,MyDateTime.getLocalTimeHumanFormat())
-            if bme != None:
-                client.publish(topic_subTemp, bme.temperature)
-                client.publish(topic_subPress, bme.pressure)
-                client.publish(topic_subHum, bme.humidity)
+            batLevel = BateryLevel_ttgo_esp32.batLevel()
+            client.publish(topic_subBat, str(batLevel))
             last_Temp = now                
         time.sleep_ms(100) # Podriamos quitarlo
 

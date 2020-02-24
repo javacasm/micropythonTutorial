07f07cf6eeb40df466883224fa5ed1d39eea5346
Javacasm <javacasm@gmail.com>
2020-02-24 13:36:12 +0100

    add

---
+++ codigo/e-paper/test-epaper.py
@@ -0,0 +1,3 @@
+# https://github.com/mcauser/micropython-waveshare-epaper
+# Wiki  https://www.waveshare.com/wiki/1.54inch_e-Paper_Module_(B)?spm=a2g0o.detail.1000023.17.1aaa6d57E4Jstg&file=1.54inch_e-Paper_Module_(B)#C_.28Used_for_Jetson_Nano.E3.80.81Raspberry_Pi.E3.80.81STM32.29
+# Producto https://es.aliexpress.com/item/32827164401.html

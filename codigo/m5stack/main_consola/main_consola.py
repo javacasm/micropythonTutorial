5dbd70453bb8c3f96f6edd73a2c074f398d9c6fc
javacasm <javacasm@gmail.com>
2021-01-18 10:07:46 +0100

    upadte

--- codigo/m5stack/main_consola/main_consola.py
+++ codigo/m5stack/main_consola/main_consola.py
@@ -5,44 +5,30 @@ from m5stack import *
 from m5ui import *
 import deviceCfg
 import time
+import neopixel
 
 import BME280  # Importamos la clase BME280
+import MQTT_base
+import NeoPixelTHO
 import config
 
-v = '0.9.6'
+
+v = '0.9.15'
 moduleName = 'main_consola'
 
 from Utils import identifyModule, myLog
 identifyModule(moduleName, v)
 
-BME280_ADDRESS = 118
+### NEOPIXEL
 
-bme = None
-def initBME280():
-    global bme
-    try:
-        i2c = machine.I2C(sda = machine.Pin(21),scl = machine.Pin(22)) # configuramos el acceso al bus i2c 
-        print(i2c.scan()) # Comprobamos que se detecta el dispositivo en la direccion 0x76 (118) 
-        bme = BME280.BME280(i2c = i2c, address = BME280_ADDRESS)
-        myLog('BME280 found @ ' + str(BME280_ADDRESS))
-    except  Exception as e:
-        myLog('BME280 not found: ' + str(e), saveToFile = True)
-        bme = None
+# Indicador NeoPixel
+neoP =  neopixel.NeoPixel(machine.Pin(config.pin_neoPixel),1)
 
-def testBME280():
-    global bme
-    if bme == None:
-        initBME280()
-    if bme != None:
-        myLog('Temp: '+str(bme.temperature) + ' Pres: '+ str(bme.pressure) + ' Hum: '+str(bme.humidity))
+def showLedRGBColor(color):
+    neoP[0]=NeoPixelTHO.colorByName(c)
+    neoP.write()
 
-
-def testBME280Wireless():
-    global bme
-    if bme == None:
-        initBME280()
-    if bme != None:
-        myLog(bme.temperature) 
+### UI
 
 labelT = None
 labelP = None
@@ -55,21 +41,17 @@ def initUI():
     labelP = M5TextBox(0, 85, "P: --- " , lcd.FONT_DejaVu40, 0x88FF88, rotate=0)
     labelH = M5TextBox(0, 130, "H: --- ", lcd.FONT_DejaVu40, 0xFF55FF, rotate=0)
 
-def getDataBME280():
-    return (bme.temperature, bme.pressure, bme.humidity)
-
-def uiTemp():
+def showUIData():
     global bme
     global labelT, labelP, labelH
+    global temp, press, hum, strData
     
     if bme == None:
         initBME280()
     lcd.clear()
-    global labelT, labelP, labelH
     if labelT == None:
         initUI()
     if bme != None:
-        temp, press, hum = getDataBME280()
         strTemp = str(temp)        
         labelT.setText(strTemp)
         myLog('T: ' + strTemp)
@@ -77,13 +59,49 @@ def uiTemp():
         labelH.setText("H: " + str(hum))
     else:
         myLog('No sensor')    
+
+
+### Sensores BME280
+
+bme = None
+
+temp = press = hum = strData = None
+
+def initBME280():
+    global bme
+    try:
+        #i2c = machine.I2C(sda = machine.Pin(21),scl = machine.Pin(22)) # configuramos el acceso al bus i2c
+        i2c = machine.I2C(sda = machine.Pin(config.pin_SDA),scl = machine.Pin(config.pin_SDL)) # configuramos el acceso al bus i2c 
+        print(i2c.scan()) # Comprobamos que se detecta el dispositivo en la direccion 0x76 (118) 
+        bme = BME280.BME280(i2c = i2c, address = config.BME280_ADDRESS)
+        myLog('BME280 found @ ' + str(config.BME280_ADDRESS))
+    except  Exception as e:
+        myLog('BME280 not found: ' + str(e), saveToFile = True)
+        bme = None
+
+def testBME280():
+    global bme
+    global temp, press, hum, strData
     
+    if bme == None:
+        initBME280()
+    if bme != None:
+        temp, press, hum = getDataBME280()
+        strData = 'Temp: '+str(temp) + ' Pres: '+ str(press) + ' Hum: '+str(hum)
+        myLog(strData)
+
+def getDataBME280():
+    return (bme.temperature, bme.pressure, bme.humidity)
+
+
+### WIFI
+
 def testWifi():
     wl = network.WLAN(network.STA_IF)
     wl.active(True)
     
     if wl.isconnected():
-        print('Wifi - Connected')
+        print('Wifi - Connected to ' + wl.config('essid'))
         lcd.image(0, 200, '/flash/img/wifi-48.jpg', type=lcd.JPG)
     else:
         print('connecting')
@@ -99,17 +117,50 @@ def testWifi():
             wl.connect(ssid, pwd)
             lcd.image(0, 200, '/flash/img/wifi-48.jpg', type=lcd.JPG)
 
+### MQTT
+
+def sub_CheckTopics(topic, msg):
+    myLog(topic + ' : ' + msg)
+    
+    if topic == topic_subLedRGB:      ## Check for RGB Topic
+        showLedRGBColor(msg)
+"""    if topic == topic_subLed:     # Check for Led Topic
+        if msg == b'On':
+            print('Led:On')
+            MeteoSalon.led.off()
+        else:
+            print('Led:Off')
+            MeteoSalon.led.on()
+    elif topic == topic_subLedRGB:      ## Check for RGB Topic
+        MeteoSalon.color(msg)
+    elif topic == topic_subFree:        ## Check for free memory
+        freeMem = helpFiles.free()
+        client.publish(topic_subMem, str(freeMem))"""
+
+
+def publishMQTTData():
+    temp, press, hum = getDataBME280()
+    msgTime = MyDateTime.getLocalTimeHumanFormat()
+    MQTT_base.client.publish(config.topic_subTemp, temp)
+    MQTT_base.client.publish(config.topic_subPress, press)
+    MQTT_base.client.publish(config.topic_subHum, hum)
+    msg = msgTime+', '+str(temp)+', '+press+', '+hum
+    MQTT_base.client.publish(config.topic_subData, msg)  
 
 def run():
     testWifi()
 
+    MQTT_base.connect_and_subscribe() # connect and get a client reference    
+
     uiTemp()
     last_Beat = int(round(time.time() * 1000))
     while True:
+        MQTT_base.client.check_msg() # Check por new messages and call the callBack function        
         now = int(round(time.time() * 1000))
         if (now - last_Beat) > config.tRefresco: # 60 segundos
+            publishMQTTData()
+            showUIData()
             last_Beat = now
-            uiTemp()
         if btnC.wasPressed():
             uiTemp()
         if btnB.wasPressed():

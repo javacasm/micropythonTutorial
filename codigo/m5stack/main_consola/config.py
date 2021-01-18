c518f5100c235ac15ef45c0d449ed347ef4bcde7
javacasm <javacasm@gmail.com>
2021-01-18 10:07:31 +0100

    upadte

--- codigo/m5stack/main_consola/config.py
+++ codigo/m5stack/main_consola/config.py
@@ -1,12 +1,34 @@
 # Config file
 
-v = '0.1.1'
+v = '0.1.7'
 moduleName = 'config'
 
 from Utils import identifyModule, myLog
 identifyModule(moduleName, v)
 
 
-tRefresco = 5000
+tRefresco = 30000
 
 mqtt_server = '192.168.1.88'
+
+pin_neoPixel = 2
+
+pin_SDA = 21
+pin_SDL = 22
+
+BME280_ADDRESS = 118
+
+# Topics
+
+topic_sub = b'MeteoSalon'
+topic_subTime = topic_sub + b'/Time'
+topic_subTemp = topic_sub + b'/Temp'
+topic_subHum = topic_sub + b'/Hum'
+topic_subPress = topic_sub + b'/Press'
+topic_subLedRGB = topic_sub + b'/ledRGB'
+topic_subData = topic_sub + b'/SensorData'
+topic_subCalderaAction = topic_sub + b'/caldera'
+topic_subCalderaStatus = topic_sub + b'/calderaStatus'
+
+msg_calderaOn = 'On'
+msg_calderaOff = 'Off'

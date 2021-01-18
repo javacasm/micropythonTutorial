69b7cad851477c9c139ee774b5cbe97cfc0e7aa9
Javacasm <javacasm@gmail.com>
2021-01-18 22:35:33 +0100

    update

--- codigo/m5stack/main_consola/main.py
+++ codigo/m5stack/main_consola/main.py
@@ -13,8 +13,7 @@ import NeoPixelTHO
 import config
 import MyDateTime
 
-   
-v = '1.0.4'
+v = '1.1.21'
 moduleName = 'main_consola'
 
 from Utils import identifyModule, myLog
@@ -40,16 +39,16 @@ def testLed():
 labelT = None
 labelP = None
 labelH = None
-
+bCalderaOn = None
 def testUI():
     myLog('Black background')
     setScreenColor(0x000000)
-    myLog('Circle', level = 2 )
-    lcd.circle(147,201,40, 0xd7ceca,0xf5be2b)
     myLog('image', level = 2 )
     lcd.image(100, 100, '/flash/img/paper_128.jpg', type=lcd.JPG)
+    myLog('Circle', level = 2 )
+    lcd.circle(130,201,60, 0xd7ceca,0xf5be2b)
     myLog('Label', level = 2 )
-    label0 = M5TextBox(10, 10, "Heart Rate", lcd.FONT_DejaVu56, 0xFFFFFF, rotate=0)
+    label0 = M5TextBox(10, 10, "Consola", lcd.FONT_DejaVu56, 0xFFFFFF, rotate=0)
     myLog('speaker', level = 2 )
     speaker.tone(880, 120, 1)
 
@@ -57,17 +56,25 @@ def clearUI():
     lcd.clear()
 
 def initUI():
-    global labelT, labelP, labelH
     myLog('Init UI', level = 1)
-    labelT = M5TextBox(0, 0, "T: --- ", lcd.FONT_DejaVu72, 0x88FFFF, rotate=0)
-    labelP = M5TextBox(0, 85, "P: --- " , lcd.FONT_DejaVu40, 0x88FF88, rotate=0)
-    labelH = M5TextBox(0, 130, "H: --- ", lcd.FONT_DejaVu40, 0xFF55FF, rotate=0)
     myLog('Initted UI', level = 1)
+
+def myPrint(x, y , texto, fuente,color):
+    lcd.font(fuente)
+    # w,h=lcd.fontSize()
+    # lcd.setColor(color)
+    # lcd.clearwin(w*lenLabel,h)
+    lcd.text(x,y, texto, color = color)
+
+def marco(x,y,w,h,grosor,color):
+    for i in range(0,grosor):
+        lcd.rect(x+i,y+i,w-2*i,h-2*i,color=color)
 
 def showUIData():
     global bme
     global labelT, labelP, labelH
     global temp, press, hum, strData
+    global bCalderaOn
     myLog('Show UI Data', level = 1)
     if bme == None:
         initBME280()
@@ -76,10 +83,22 @@ def showUIData():
     if bme != None:
         lcd.clear()
         strTemp = str(temp)        
-        labelT.setText(strTemp)
+        myPrint(0, 0, strTemp, lcd.FONT_DejaVu72, 0x88FFFF)
         myLog('T: ' + strTemp)
-        labelP.setText("P: " + str(press))
-        labelH.setText("H: " + str(hum))
+        myPrint(0, 75, "P: " + str(press) , lcd.FONT_DejaVu40, 0x88FF88)
+        myPrint(0, 115, "H: " + str(hum), lcd.FONT_DejaVu40, 0xFF55FF)
+        text = ' - '
+        color = 0xffffff
+        if bCalderaOn == True:
+            color = 0xf54242
+            text = 'On'
+        elif bCalderaOn == False :
+            color = 0x20C8F5
+            text = 'Off'
+        myPrint(5, 168, text, lcd.FONT_DejaVu72, color)
+        marco(0, 165, 120, 70, 5, color)
+
+        
         # lcd.circle(48, 200, 30, 0xd7ceca,0xf54220)
         lcd.circle(163, 200, 30, 0xd7ceca,0x20C8F5)
         lcd.circle(258, 200, 30, 0xd7ceca,0xf54220)         
@@ -151,11 +170,24 @@ def testWifi():
 ### MQTT
 
 def MySubCheckTopics(topic, msg):
+    global bCalderaOn
     myLog(str(topic) + ' : ' + str(msg))
     
     if topic == config.topic_subLedRGB:      ## Check for RGB Topic
         showLedRGBColor(msg)
-       
+    elif topic == config.topic_subCalderaStatus:
+        if msg == b'On':
+            if bCalderaOn != True:
+                showUIData()
+                bCalderaOn = True
+            myLog('StatusCaldera: On')
+                
+        else:
+            if bCalderaOn != False:
+                showUIData()
+                bCalderaOn = False
+            myLog('StatusCaldera: Off')
+
 """    if topic == topic_subLed:     # Check for Led Topic
         if msg == b'On':
             print('Led:On')
@@ -180,37 +212,49 @@ def publishMQTTData():
     MQTT_base.client.publish(config.topic_subHum, hum)
     msg = msgTime+', ' + strData
     MQTT_base.client.publish(config.topic_subData, msg)
+    MQTT_base.client.publish(config.topic_subConsolaStatus, 'On')
     myLog('Published: ' + msg);
+    
 
 ### RUN
 
 def test():
     testWifi()
     testBME280()
-    testUI()
-    clearUI()
+    #testUI()
     testLed()
 
 def sendCalderaOff():
-    MQTT_base.client.publish(config.topic_subData, config.msg_calderaOff)
+    MQTT_base.client.publish(config.topic_subCalderaAction, config.msg_calderaOff)
     myLog('Sent Caldera Off', saveToFile = True)
     speaker.tone(880, 120, 1)
     
 def sendCalderaOn():
-    MQTT_base.client.publish(config.topic_subData, config.msg_calderaOn)
+    MQTT_base.client.publish(config.topic_subCalderaAction, config.msg_calderaOn)
     myLog('Sent Caldera On', saveToFile = True)
     speaker.tone(440, 120, 1)
 
 
+def wait4Wifi(tiempo):
+    for i in range(0,tiempo):
+        getSensorData()
+        myPrint(0, 0, str(temp), lcd.FONT_DejaVu72, 0x88FFFF)
+        myPrint(130, 120, str(5 - i) , lcd.FONT_DejaVu40, 0x88FF88)
+        marco(122,116,40,40,5,0x88FF88)
+        time.sleep(1)
+        testLed()
+        clearUI()   
+
 def run():
     test()
-    myLog('Waiting 10 seconds...')
-    time.sleep(10)
     
-    MQTT_base.connect_and_subscribe(config.topic_subLedRGB, MySubCheckTopics) # connect and get a client reference    
+    myLog('Waiting 5 seconds for wifi setup...')
+    wait4Wifi(5)
+    
+    MQTT_base.connect_and_subscribe((config.topic_subLedRGB, config.topic_subCalderaStatus), MySubCheckTopics) # connect and get a client reference    
 
     last_Beat = 0 # int(round(time.time() * 1000))
-    
+    MQTT_base.client.publish(config.topic_subInitConsola,'On')
     while True:
         try:
             MQTT_base.client.check_msg() # Check por new messages and call the callBack function        
@@ -229,6 +273,5 @@ def run():
         except Exception as e:
             myLog('Main exception: ' + str(e), saveToFile = True)
 
-
-
-run()
+if __name__ == '__main__':
+    run()

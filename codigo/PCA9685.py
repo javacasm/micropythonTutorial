e370d1ccff0eb32e85edc74fd497b529d1a2ebce
javacasm <javacasm@gmail.com>
2021-01-21 20:33:23 +0100

    update

---
+++ codigo/PCA9685.py
@@ -0,0 +1,7 @@
+# PCA9685 - Servo & PWM driver
+"""
+[Adafruit guide](https://learn.adafruit.com/micropython-hardware-pca9685-pwm-and-servo-driver/micropython)
+[Adafruit code](https://github.com/adafruit/micropython-adafruit-pca9685)
+[Documentation](http://micropython-pca9685.readthedocs.io/)
+
+"""

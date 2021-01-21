6b47121e944352424e0a8aed6011782f3509c10e
javacasm <javacasm@gmail.com>
2021-01-21 13:40:45 +0100

    update

---
+++ codigo/RotaryEncoder.py
@@ -0,0 +1,7 @@
+# Rotary Encoder in micropython
+"""
+https://github.com/SpotlightKid/micropython-stm-lib/tree/master/encoder
+https://github.com/miketeachman/micropython-rotary
+
+discussion https://forum.micropython.org/viewtopic.php?t=2356
+"""
--- codigo/moodLamp/test_lamp.py
+++ codigo/moodLamp/test_lamp.py
@@ -2,12 +2,37 @@ import machine
 from neopixel import NeoPixel
 import Wemos
 import BME280_test
+import time
 
-BME280_test.testBME280Wireless()
+v = '0.2'
 
-np = NeoPixel(machine.Pin(Wemos.D7),17)
+N_Led = 16
 
-for i in range(0,17):
-    np[i]=(255,255,255,)
-    
-np.write()
+def testBME():
+    BME280_test.testBME280Wireless()
+
+np = NeoPixel(machine.Pin(Wemos.D2), N_Led)
+
+def testRGBWhite():
+    for i in range(0, N_Led):
+        np[i]=(255, 255, 255)
+    np.write()
+
+def testFadeOutRGB():
+    for t in range(0,10):
+        for i in range(0, N_Led):
+            np[i]=(155-15*t, 155-15*t, 155-15*t)
+        np.write()
+        time.sleep(0.1)
+    RGBoff()
+
+def RGBoff():
+    for i in range(0, N_Led):
+        np[i]=(0, 0, 0)
+    np.write()
+
+
+testBME()
+testRGBWhite()
+time.sleep(1)
+testFadeOutRGB()
